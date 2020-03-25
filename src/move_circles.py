edb4f2cbd0d71b9d8bf4da45b59f9a310bcd1804
Remco Kuijpers <remco.kuijpers@hotmail.nl>
2020-03-25 13:15:17 +0100

    Changed circle move to work with DeltaRobot class

--- src/move_circles.py
+++ src/move_circles.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python
 
-from kinematics import DeltaRobotKinematics, DeltaPositionError
 import rospy
 import time
-from geometry_msgs.msg import Vector3
-from math import sin, cos, radians
+from math import sin, cos
+from delta_robot import DeltaRobot
 
 if __name__ == "__main__":
-    rospy.init_node("circle_publisher")
-    pub = rospy.Publisher("/delta_robot/pos_cmd", Vector3, queue_size=10)
-    a = Vector3()
-    kin = DeltaRobotKinematics()
-    z = -800
+    robot = DeltaRobot()
+    z = -700
     while not rospy.is_shutdown():
         try:
-            x = 100*sin(time.time())
-            y = 100*cos(time.time())
-            m1, m2, m3 = kin.reverse(x,y,z)
-            a.x, a.y, a.z = radians(m1), radians(m2), radians(m3)
-            pub.publish(a)
+            x = 100*sin(time.time()*2)
+            y = 100*cos(time.time()*2)
+            robot.movePositionCartesian(x,y,z)
         except rospy.ROSException or KeyboardInterrupt:
             break
