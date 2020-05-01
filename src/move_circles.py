892ccc80d88b9e97bd8cccd13af62dc9b514f268
Remco Kuijpers <remco.kuijpers@hotmail.nl>
2020-05-01 16:08:37 +0200

    Moving to zero

--- src/move_circles.py
+++ src/move_circles.py
@@ -6,12 +6,12 @@ from math import sin, cos
 from delta_robot import DeltaRobot
 
 if __name__ == "__main__":
-    robot = DeltaRobot()
+    robot = DeltaRobot("delta_robot")
     z = -700
     while not rospy.is_shutdown():
         try:
-            x = 100*sin(time.time()*2)
-            y = 100*cos(time.time()*2)
+            x = 0#100*sin(time.time()*2)
+            y = 0#100*cos(time.time()*2)
             robot.movePositionCartesian(x,y,z)
         except rospy.ROSException or KeyboardInterrupt:
             break
