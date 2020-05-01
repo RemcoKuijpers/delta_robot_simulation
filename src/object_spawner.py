a170cb1b5e4172a1014c4b9f13bc173f1e48f90c
Remco Kuijpers <remco.kuijpers@hotmail.nl>
2020-05-01 16:08:59 +0200

    Updated for position control

--- src/object_spawner.py
+++ src/object_spawner.py
@@ -8,6 +8,8 @@ import time
 import random
 from gazebo_msgs.srv import DeleteModel, SpawnModel
 from geometry_msgs.msg import Pose, Quaternion, Point
+from gazebo_msgs.msg import ModelState
+from gazebo_msgs.srv import SetModelState
 from math import pi
 
 class PotatoSpawner(object):
@@ -15,21 +17,55 @@ class PotatoSpawner(object):
         rospy.init_node("potato_spawner")
         rospy.wait_for_service("gazebo/delete_model")
         rospy.wait_for_service("gazebo/spawn_sdf_model")
+        rospy.wait_for_service('/gazebo/set_model_state')
         self.delete = rospy.ServiceProxy("gazebo/delete_model", DeleteModel)
         self.spawn = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
+        self.set = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
         rospack = rospkg.RosPack()
+        self.ids = []
         with open(os.path.join(rospack.get_path("delta_robot_simulation"), "urdf", "object.urdf"), "r") as f:
             self.model = f.read()
 
-    def spawnPotato(self, x_position, id):
+    def spawnPotato(self, id, x_position, y_position):
         item_name = "potato_{0}".format(id)
         quat = tf.transformations.quaternion_from_euler(0,0,random.uniform(0,2*pi))
-        pose = Pose(Point(x=x_position, y=0, z=1.1),   Quaternion(x=quat[0], y=quat[1], z=quat[2], w=quat[3]))
+        pose = Pose(Point(x=x_position, y=y_position, z=1.1),   Quaternion(x=quat[0], y=quat[1], z=quat[2], w=quat[3]))
         self.spawn(item_name, self.model, "", pose, "world")
+
+    def spawnManyPotatos(self, n):
+        for id in range(n):
+            self.spawnPotato(id, 0, 0)
+            self.ids.append(id)
 
     def deletePotato(self, id):
         item_name = "potato_{0}".format(id)
         self.delete(item_name)
+        
+    def updatePose(self, id, x_position, y_position):
+        if id not in self.ids:
+            #self.spawnPotato(id, x_position, y_position)
+            #self.ids.append(id)
+            #print("Spawned potato {0}".format(id))
+            pass
+        else:
+            msg = ModelState()
+            msg.model_name = "potato_{0}".format(id)
+            msg.pose.position.y = y_position
+            msg.pose.position.x = x_position
+            msg.pose.position.z = 1.1
+            msg.pose.orientation.x = 0
+            msg.pose.orientation.y = 0
+            msg.pose.orientation.z = 0
+            msg.pose.orientation.w = 0
+            msg.twist.linear.x = 0
+            msg.twist.linear.y = 0
+            msg.twist.linear.z = 0
+            msg.twist.angular.x = 0
+            msg.twist.angular.y = 0
+            msg.twist.angular.z = 0
+            msg.reference_frame = "world"
+            self.set(msg)
+            #print("Updated pose {0}".format(id))
 
 if __name__ == "__main__":
     s = PotatoSpawner()
--- src/tcp_objects.py
+++ src/tcp_objects.py
@@ -1,43 +1,36 @@
 #!/usr/bin/env python
 
 import socket
-import time
 import re
+import time
 from object_spawner import PotatoSpawner
 
-class TCPServerObjectSpawner(object):
+class TCPServerObjectSpawner():
     def __init__(self):
         TCP_IP = '10.139.10.6'
         TCP_PORT = 5006
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((TCP_IP, TCP_PORT))
-        s.listen(1)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.s.bind((TCP_IP, TCP_PORT))
+        #s.listen(1)
         print("Waiting for client to connect...")
-        self.conn, addr = s.accept()
-        print("Client connected: ", addr)
-        self.listen()
+        #self.conn, addr = s.accept()
+        #print("Client connected: ", addr)
+        self.p = PotatoSpawner()
+        self.p.spawnManyPotatos(30)
 
     def listen(self):
-        self.p = PotatoSpawner()
-        i = 1
-        while True:
-            BUFFER_SIZE = 1000
-            data = self.conn.recv(BUFFER_SIZE)
-            if not data: break
-            data = float(re.findall(r'\d+', data.decode())[0])/1000
-            self.p.spawnPotato(data, i)
-            i += 1
-
-def twos_comp(val, bits):
-    """compute the 2's complement of int value val"""
-    if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
-        val = val - (1 << bits)        # compute negative value
-    return val     
-
+        data = self.s.recvfrom(81)
+        data = re.findall(r'-?\d+', data[0])
+        if data != []:
+            try:
+                num = int(data[0])
+                x = float(data[1])/1000
+                y = float(data[2])/1000
+                self.p.updatePose(num,x,y)
+            except IndexError:
+                pass
+                
 if __name__ == "__main__":
     obj = TCPServerObjectSpawner()
-    try:
+    while True:
         obj.listen()
-    except KeyboardInterrupt:
-        for i in range(1,1501):
-            obj.p.deletePotato(i)
