d4e699d01c8987974972cde973083d05a0a4b690
Remco Kuijpers <remco.kuijpers@hotmail.nl>
2020-06-10 12:29:02 +0200

    Added code for manual positioning

--- src/move_circles.py
+++ src/move_circles.py
@@ -6,13 +6,18 @@ from math import sin, cos
 from delta_robot import DeltaRobot
 
 if __name__ == "__main__":
-    robot = DeltaRobot("delta_robot")
-    z = -700
+    robot1 = DeltaRobot("delta_robot")
+    robot2 = DeltaRobot("delta_robot2")
+    z = -800
     while not rospy.is_shutdown():
         try:
+            #x = int(input('X:'))
+            #y = int(input('Y:'))
+            #z = int(input('Z:'))
             x = 200*cos(time.time()*4)
             y = 200*sin(time.time()*4)
             aux = 720*sin(time.time())
-            robot.movePositionCartesian(x,y,z,aux)
+            robot1.movePositionCartesian(x,y,z,aux)
+            robot2.movePositionCartesian(x,y,z,aux)
         except rospy.ROSException or KeyboardInterrupt:
             break
